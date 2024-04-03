75867fb8becb6c6d7a8b1ba6cea5fbd389b73b70
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-04-03 20:40:54 -0300

    feat: add users register

--- src/Routes/Users.ts
+++ src/Routes/Users.ts
@@ -1,4 +1,24 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { client } from "../Database/Mongo";
 const users = express.Router();
 
+// register
+users.post('/', async (request: Request, response: Response, nextFunction: NextFunction) => {
+  // <TO-DO> Authentication is required, Check if user is authenticate
+  // <TO-DO> Apply implements user interface
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashedPassword = bcrypt.hashSync(request.body.password, salt);
+
+  const user = {
+    username: request.body.username,
+    email: request.body.email,
+    password: hashedPassword
+  }
+
+  await client.db('admin').collection('users').insertOne(user);
+
+  return response.send(user);
+})
 
 export { users };
